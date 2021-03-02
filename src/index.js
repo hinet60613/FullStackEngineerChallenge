1004ce0434aec6080aa862e3dc7770f213703ee7
Hinet60613 <hinet60613@gmail.com>
2021-03-02 12:16:54 +0800

    replace default App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './Components/App';
 import './index.css';
-import App from './App';
+//import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
