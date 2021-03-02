c5a8b4a3d220fd35b1ed38db6fdf02fed94fc025
Hinet60613 <hinet60613@gmail.com>
2021-03-02 14:01:25 +0800

    crreate PerfReview from array of reviews

--- src/Components/PerfReview/index.js
+++ src/Components/PerfReview/index.js
@@ -9,10 +9,34 @@ const PerfReviewFeedback = (props) => {
 }
 
 const PerfReview = (props) => {
+    const reviews = [
+        {
+            reviewer: "Alice",
+            rating: 4.9,
+            comment: "Always a pleasure to work with you!"
+        },
+        {
+            reviewer: "Bob",
+            rating: 3.0,
+            comment: "Could reply email more often."
+        },
+    ];
+
+    // since we are only appending new data to the end of array, it's safe to use index as key.
     return (
         <div>
-            <PerfReviewFeedback reviewer="Alice" rating={4.9} comment="Always a pleasure to work with you!" />
-            <PerfReviewFeedback reviewer="Bob" rating={3.0} comment="Could reply email more often." />
+            {
+                reviews.map(
+                    ({ reviewer, rating, comment }, index) => (
+                        <PerfReviewFeedback
+                            key={index}
+                            reviewer={reviewer}
+                            rating={rating}
+                            comment={comment}
+                        />
+                    )
+                )
+            }
         </div>
     )
 }
