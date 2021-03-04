016f1544f85b2506fbbf25c05dea1d3d2460f93e
Hinet60613 <hinet60613@gmail.com>
2021-03-04 21:03:09 +0800

    extract item list fetching from ListBase.

--- src/components/PerfReviewList/index.js
+++ src/components/PerfReviewList/index.js
@@ -2,33 +2,48 @@ import { Link } from 'react-router-dom';
 import { withAuth } from '../Session';
 import * as ROUTES from '../../constants/routes';
 
-const PerfReviewListBase = (props) => {
-    const item_list = props.auth.doGetPerfReviewList("Alice").result;
-    console.log({ item_list });
-    return (
-        <ul>
-            {
-                item_list.map(
-                    ({ id, target, reviewer }, idx) => (
-                        <li key={idx}>
-                            <Link to={ROUTES.PERF_FEEDBACK + `/${id}/${reviewer}/${target}`}>
-                                [{target}] from {reviewer}
-                            </Link>
-                        </li>
-                    )
+const PerfReviewListBase = (props) => (
+    <ul>
+        {
+            props.review_list.map(
+                ({ id, target, reviewer }, idx) => (
+                    <li key={idx}>
+                        <Link to={ROUTES.PERF_FEEDBACK + `/${id}/${reviewer}/${target}`}>
+                            [{target}] from {reviewer}
+                        </Link>
+                    </li>
                 )
-            }
-        </ul>
-    );
-}
+            )
+        }
+    </ul>
+)
 
 const PerfReviewList = withAuth(PerfReviewListBase);
+
+const ReviewerPendingPerfReviewListBase = (props) => {
+    const { reviewer } = props;
+    const item_list = props.auth.doGetPerfReviewListByReviewer(reviewer).result || [];
+    return <PerfReviewListBase review_list={item_list} />
+};
+
+const ReviewerPendingPerfReviewList = withAuth(ReviewerPendingPerfReviewListBase);
+
+const TargetPerfReviewListBase = (props) => {
+    const { target } = props;
+    const item_list = props.auth.doGetPerfReviewList(target).result || [];
+    return <PerfReviewListBase review_list={item_list} />;
+}
+const TargetPerfReviewList = withAuth(TargetPerfReviewListBase);
+//const PerfReviewList = withAuth(PerfReviewListBase);
 
 const PerfReviewListPage = ({ authUser }) => {
     return (
         <div>
             <Link to={ROUTES.NEW_PERF_REVIEW}>Create</Link>
-            <PerfReviewList />
+            <h2>Performance review list for Alice</h2>
+            <TargetPerfReviewList target="Alice" />
+            <h2>Pending performance review list for Bob</h2>
+            <ReviewerPendingPerfReviewList reviewer="Bob" />
         </div>
     );
 }
