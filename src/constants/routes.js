c423e716b8bbd81d69be58312a7b4773b168ab77
Hinet60613 <hinet60613@gmail.com>
2021-03-04 13:59:16 +0800

    add routes constant

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const LANDING = '/';
+export const PERF_REVIEW_LIST = '/perf_review_list';
+export const PERF_REVIEW = '/perf_review';
+export const NEW_PERF_REVIEW = '/perf_review/new';
+export const PERF_FEEDBACK = '/perf_feedback';
+export const PENDING_FEEDBACK = '/pending_feedback';
+export const API = '/api';
