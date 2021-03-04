4dffd2ccd5add46cfda534afee8dc72a32fd299d
Hinet60613 <hinet60613@gmail.com>
2021-03-04 14:52:57 +0800

    fix no return problem for AuthContext

--- src/components/Session/context.js
+++ src/components/Session/context.js
@@ -1,8 +1,8 @@
 import React from 'react';
 const AuthContext = React.createContext(null);
-export default withAuth = Component => props => {
+export const withAuth = Component => props => (
     <AuthContext.Consumer>
         {auth => <Component {...props} auth={auth} />}
     </AuthContext.Consumer>
-}
+);
 export default AuthContext;
