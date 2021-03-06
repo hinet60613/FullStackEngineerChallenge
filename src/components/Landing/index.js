84959872e5f133c8697b143320af1f5453f1788a
Hinet60613 <hinet60613@gmail.com>
2021-03-07 00:13:42 +0800

    make landing page response to auth status.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,29 @@
-const Landing = () => (
-    <div>
-        <h1>Landing</h1>
-    </div>
-);
+import { withAuth } from "../Session";
+import * as ROUTES from '../../constants/routes';
+import { Link } from "react-router-dom";
+import { Component } from "react";
+
+const LandingWithAuth = (props) => (
+    <h1>Hi {props.user.display_name}</h1>
+)
+
+const LandingWithoutAuth = () => (
+    <h1>Greetings. Please <Link to={ROUTES.SIGN_IN}>sign in.</Link></h1>
+)
+
+class LandingBase extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        console.log({ props: this.props });
+        const user = this.props.auth?.state?.auth;
+        return (<div>
+            {user ? <LandingWithAuth user={user} /> : <LandingWithoutAuth />}
+        </div>);
+    }
+};
+
+const Landing = withAuth(LandingBase);
 
 export default Landing;
