ddd0adfe9bb6d5b7bd0723119d39739fdd7a901c
Hinet60613 <hinet60613@gmail.com>
2021-03-07 00:14:25 +0800

    switch App component to class component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,25 +5,47 @@ import * as ROUTES from '../../constants/routes';
 import Navigator from "../Navigation";
 import Landing from "../Landing";
 import PerfReviewListPage from "../PerfReviewList";
+import { withAuth } from "../Session";
+import SignInPage from "../SignIn";
+import { Component } from "react";
 
-const App = () => (
-    <Router>
-        <div>
-            <Navigator />
-            <hr />
-            <Route path={ROUTES.LANDING} component={Landing} />
-            <Switch>
-                <Route path={ROUTES.NEW_PERF_REVIEW} component={NewPerfReviewFormBase} />
-                <Route path={ROUTES.PERF_REVIEW} component={PerfReview} />
-                <Route
-                    path={ROUTES.PERF_FEEDBACK + "/:id/:reviewer/:target"}
-                    component={PerfReviewFeedbackFormBase}
-                />
-                <Route path={ROUTES.PERF_REVIEW_LIST} component={PerfReviewListPage} />
-                <Route path={ROUTES.PENDING_FEEDBACK} component={PerfReviewList} />
-            </Switch>
-        </div>
-    </Router>
-);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authUser: null,
+        }
+    }
 
-export default App;
+    componentDidUpdate() {
+        console.log("App did update.");
+    }
+    componentWillUnmount() {
+        console.log("component did unmount");
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Navigator />
+                    <hr />
+                    <Route exact path={ROUTES.LANDING} component={Landing} />
+                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+                    <Switch>
+                        <Route path={ROUTES.NEW_PERF_REVIEW} component={NewPerfReviewFormBase} />
+                        <Route path={ROUTES.PERF_REVIEW} component={PerfReview} />
+                        <Route
+                            path={ROUTES.PERF_FEEDBACK + "/:id/:reviewer/:target"}
+                            component={PerfReviewFeedbackFormBase}
+                        />
+                        <Route path={ROUTES.PERF_REVIEW_LIST} component={PerfReviewListPage} />
+                        <Route path={ROUTES.PENDING_FEEDBACK} component={PerfReviewList} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default withAuth(App);
