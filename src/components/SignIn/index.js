e678c2f9ca9455533b61605497c919ed12598220
Hinet60613 <hinet60613@gmail.com>
2021-03-07 00:15:10 +0800

    allow user to submit user name by input field.

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import Backend, { withAuth } from "../Session";
+
 const SignInPage = () => (
     <div>
         <SignInForm />
     </div>
 );
 
-const SignInFormBase = () => {
+const SignInFormBase = (props) => {
+    const [userName, setUserName] = useState("");
+    const signInAdmin = () => props.auth.doSignInAdmin();
+    const signIn = () => props.auth.doSignIn(userName);
     return (
         <div>
             <div>
-                <input type="text" name="user_name" onChange={onChange} />
+                <input type="text" name="user_name" onChange={(e) => { setUserName(e.target.value); }} />
                 <button onClick={signIn}>Sign in</button>
             </div>
             or
