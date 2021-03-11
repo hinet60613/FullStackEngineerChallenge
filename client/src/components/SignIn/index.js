28401abc185ada8cf32f9ce9d25772961e54f7fe
Hinet60613 <hinet60613@gmail.com>
2021-03-11 14:12:32 +0800

    remove unused import

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Backend, { withAuth } from "../Session";
+import { withAuth } from "../Session";
 import * as ROUTES from '../../constants/routes'
 
 const SignInPage = (props) => (
