3107049a2e4338edcaeab3d53914bc460726198e
Hinet60613 <hinet60613@gmail.com>
2021-03-11 11:37:55 +0800

    fetch review list and pending review list from API.

--- client/src/components/PerfReviewList/index.js
+++ client/src/components/PerfReviewList/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { withAuth } from '../Session';
 import * as ROUTES from '../../constants/routes';
+import { useEffect, useState } from 'react';
 
 const PerfReviewListBase = (props) => (
     <ul>
@@ -20,16 +21,75 @@ const PerfReviewListBase = (props) => (
 
 const ReviewerPendingPerfReviewListBase = (props) => {
     const { reviewer } = props;
-    const item_list = props.auth.doGetPerfReviewListByReviewer(reviewer).result || [];
-    return <PerfReviewListBase review_list={item_list} />
+    const [reviewList, setReviewList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        fetch(`http://localhost:8080/review/pending/${reviewer}`)
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                throw res;
+            })
+            .then(data => {
+                setReviewList(data.result);
+            })
+            .catch(error => {
+                console.log('error: ', error.message)
+                setError(error);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    }, []);
+    if (loading) return 'loading...';
+    if (error) return `error: ${error.message}`;
+    //const item_list = props.auth.doGetPerfReviewListByReviewer(reviewer).result || [];
+    return <PerfReviewListBase review_list={reviewList} />
 };
 
 const ReviewerPendingPerfReviewList = withAuth(ReviewerPendingPerfReviewListBase);
 
 const TargetPerfReviewListBase = (props) => {
     const { target } = props;
-    const item_list = props.auth.doGetPerfReviewList(target).result || [];
-    return <PerfReviewListBase review_list={item_list} />;
+    const [reviewList, setReviewList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        /*
+        const fetchingData = async () => {
+            const res = await fetch('http://localhost:8080/review');
+            if (!res.ok) {
+                throw new Error('error fetching data');
+            }
+            const data = await response.json();
+            return data;
+        }
+        */
+        fetch('http://localhost:8080/review')
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                throw res;
+            })
+            .then(data => {
+                setReviewList(data.reviews.filter(review => review.target == target));
+            })
+            .catch(error => {
+                console.log('error while fetching', error);
+                setError(error.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return 'loading...';
+    if (error) return `error: ${error.message}`;
+    //const item_list = props.auth.doGetPerfReviewList(target).result || [];
+    return <PerfReviewListBase review_list={reviewList} />
 }
 const TargetPerfReviewList = withAuth(TargetPerfReviewListBase);
 //const PerfReviewList = withAuth(PerfReviewListBase);
