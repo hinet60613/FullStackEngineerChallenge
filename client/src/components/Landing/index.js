984303646ec944a8c4935e79ff2621137a955654
Hinet60613 <hinet60613@gmail.com>
2021-03-11 11:35:54 +0800

    fix LandingBase unused constructor by replace with functional component.

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,7 +1,6 @@
 import { withAuth } from "../Session";
 import * as ROUTES from '../../constants/routes';
 import { Link } from "react-router-dom";
-import { Component } from "react";
 
 const LandingWithAuth = (props) => (
     <h1>Hi {props.user.display_name}</h1>
@@ -11,18 +10,15 @@ const LandingWithoutAuth = () => (
     <h1>Greetings. Please <Link to={ROUTES.SIGN_IN}>sign in.</Link></h1>
 )
 
-class LandingBase extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        console.log({ props: this.props });
-        const user = this.props.auth?.state?.auth;
-        return (<div>
-            {user ? <LandingWithAuth user={user} /> : <LandingWithoutAuth />}
-        </div>);
-    }
-};
+
+const LandingBase = (props) => {
+    const user = props.auth?.state?.auth;
+    return (
+        <div>
+            {props.auth?.state?.auth ? <LandingWithAuth user={user} /> : <LandingWithoutAuth />}
+        </div>
+    );
+}
 
 const Landing = withAuth(LandingBase);
 
