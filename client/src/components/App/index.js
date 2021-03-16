2a0959366bc019d5afa835858ab87b7931db963d
Hinet60613 <hinet60613@gmail.com>
2021-03-16 11:28:50 +0800

    change App to functional component

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,5 +1,4 @@
-import PerfReview, { NewPerfReviewForm, NewPerfReviewFormBase, PerfReviewFeedbackForm, PerfReviewFeedbackFormBase } from "../PerfReview";
-import PerfReviewList from "../PerfReviewList";
+import PerfReview, { NewPerfReviewForm, PerfReviewFeedbackForm, } from "../PerfReview";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navigation from '../Navigation';
@@ -10,41 +9,27 @@ import { withAuth } from "../Session";
 import { Component } from "react";
 
 import * as ROUTES from '../../constants/routes';
+import PendingFeedbackPage from "../Feedback";
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+const App = () => (
+    <Router>
+        <div>
+            <Navigation />
+            <hr />
+            <Route exact path={ROUTES.LANDING} component={Landing} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
 
-    componentDidUpdate() {
-        console.log("App did update.");
-    }
-    componentWillUnmount() {
-        console.log("component did unmount");
-    }
+            <Switch>
+                <Route path={ROUTES.REVIEW + '/new'} component={NewPerfReviewForm} />
+                <Route path={ROUTES.REVIEW + '/:id'} component={PerfReview} />
+                <Route path={ROUTES.REVIEW} component={PerfReviewListPage} />
 
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Navigation />
-                    <hr />
-                    <Route exact path={ROUTES.LANDING} component={Landing} />
-                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+                <Route path={ROUTES.FEEDBACK + '/:id'} component={PerfReviewFeedbackForm} />
+                <Route path={ROUTES.FEEDBACK} component={PendingFeedbackPage} />
 
-                    <Switch>
-                        <Route path='/review/new' component={NewPerfReviewForm} />
-                        <Route path='/review/:id' component={PerfReview} />
-                        <Route path='/review' component={PerfReviewListPage} />
-
-                        <Route path={ROUTES.FEEDBACK + '/:id'} component={PerfReviewFeedbackForm} />
-                        <Route path={ROUTES.FEEDBACK} component={() => (<div><h2>Pending Feedback</h2></div>)} />
-
-                    </Switch>
-                </div>
-            </Router>
-        );
-    }
-}
+            </Switch>
+        </div>
+    </Router>
+);
 
 export default withAuth(App);
