a7b9ea614804503172d59fd41315bea19f5f84ce
Hinet60613 <hinet60613@gmail.com>
2021-03-16 09:56:09 +0800

    rename navigation

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,45 +1,54 @@
+import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import { withAuth } from '../Session';
 
-const NavigationBase = (props) => (
+const NavigationWithAuth = (props) => (
     <div>
-        {
-            props.backend?.auth ?
-                <NavigationAuth auth={props.backend.auth} />
-                : <NavigationNonAuth />
-        }
-    </div>
-);
-
-const Navigation = withAuth(NavigationBase);
-
-const NavigationAuth = () => (
-    <div>
-        <h1>Navigation(Auth)</h1>
-    </div>
-);
-
-const NavigationNonAuth = () => (
-    <div>
-        <h1>Navigation</h1>
+        <h1>Hi {props.user.display_name}</h1>
         <nav>
             <ul>
                 <li>
-                    <Link to={ROUTES.LANDING}>Landing</Link>
+                    <Link to={ROUTES.LANDING}>Home</Link>
                 </li>
                 <li>
-                    <Link to={ROUTES.PERF_REVIEW}>Performance Review</Link>
+                    <Link to={ROUTES.REVIEW}>Performance Review</Link>
                 </li>
                 <li>
-                    <Link to={ROUTES.PERF_REVIEW_LIST}>Performacne Review List</Link>
+                    <Link to={ROUTES.FEEDBACK}>Pending Feedback</Link>
                 </li>
+            </ul>
+        </nav>
+    </div>
+);
+
+const NavigationWithoutAuth = () => (
+    <div>
+        <h1>Please <Link to={ROUTES.SIGN_IN}>sign in.</Link></h1>
+        <nav>
+            <ul>
                 <li>
-                    <Link to={ROUTES.PENDING_FEEDBACK}>Pending Feedback</Link>
+                    <Link to={ROUTES.LANDING}>Home</Link>
                 </li>
             </ul>
         </nav>
     </div>
 )
+
+const NavigationBase = (props) => {
+    console.log('render <NavigationBase />');
+    return (
+        <div>
+            {
+                (props.auth?.state?.auth) ?
+                    <NavigationWithAuth user={props.auth.state.auth} />
+                    : <NavigationWithoutAuth />
+            }
+        </div>
+    );
+}
+
+
+const Navigation = withAuth(NavigationBase);
 
 export default Navigation;
